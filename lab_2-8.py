ee48af8671a42661c550388fee0570f709468b17
p23pylons <p23plyons@fairfieldprep.org>
2022-09-21 14:04:34 -0400

    Submission of cycle 2 day 6 labs

---
+++ lab_2-8.py
@@ -0,0 +1,13 @@
+# Author: PCL 09/21/22
+score = 17
+if score >= 15:
+    print("Gold medal")
+else:
+    if 14 >= score >= 12:
+        print("Silver medal")
+    else:
+        if 11 >= score >= 9:
+            print("Bronze medal")
+        else:
+            if score <= 8:
+                print("No medal")
---
+++ lab_2-9.py
@@ -0,0 +1,10 @@
+# Author: PCL 09/21/22
+score = 17
+if score >= 15:
+    print("Gold medal")
+elif 14 >= score >= 12:
+    print("Silver medal")
+elif 11 >= score >= 9:
+    print("Bronze medal")
+else:
+    print("No medal")
